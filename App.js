33a413e323f2a316c98cf7ae5d962589486e2c23
jgilligan113 <jgilligan.sav@gmail.com>
2017-12-01 20:08:01 -0500

    added a good few navigable pages using stack and next up is tabs

--- App.js
+++ App.js
@@ -2,15 +2,25 @@ import React from 'react';
 import { StyleSheet, Text, View, Button } from 'react-native';
 import { StackNavigator } from 'react-navigation';
 
-class HomeScreen extends React.Component {
+class WelcomeScreen extends React.Component {
   static navigationOptions = {
     title: 'Welcome'
   };
   render() {
   const { navigate } = this.props.navigation;
   return (
-    <View>
-      <Text>Savannah Tour of Homes and Gardens</Text>
+    
+      <View style={{
+            flex: 1,
+            flexDirection: 'row',
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}>
+            <Image 
+              source={require("../imgs/STHG_logo_green.png")}
+              style={{ height: 450, width: 320 }}
+            />
+      {/* <Text>Savannah Tour of Homes and Gardens</Text> */}
       <Button
           onPress={() => navigate('User', {user: 'Molly'})}
           title="User"
@@ -22,6 +32,14 @@ class HomeScreen extends React.Component {
       <Button
           onPress={() => navigate('Map')}
           title="Tour Map"
+      />
+      <Button
+          onPress={() => navigate('Homes')}
+          title="Homes"
+      />
+      <Button
+          onPress={() => navigate('Sponsors')}
+          title="Sponsors"
       />
     </View>
     );
@@ -56,6 +74,49 @@ class MapScreen extends React.Component {
   }
 }
 
+class SponsorsScreen extends React.Component {
+  static navigationOptions = ({ navigation }) => { 
+    title: 'Sponsors'
+  };
+  render() {
+  const { params } = this.props.navigation.state;
+  return (
+    <View>
+      <Text>List of sponsors with links to external sites? coupon codes or specials listed?</Text>
+    </View>
+    );
+  }
+}
+
+class HomesScreen extends React.Component {
+  static navigationOptions = ({ navigation }) => { 
+    title: 'Homes'
+  };
+  render() {
+  const { params } = this.props.navigation.state;
+  return (
+    <View>
+      <Text>Homes with images and cards possibly</Text>
+    </View>
+    );
+  }
+}
+
+class FavoritesScreen extends React.Component {
+  static navigationOptions = ({ navigation }) => { 
+    title: 'Favorites'
+  };
+  render() {
+  const { params } = this.props.navigation.state;
+  return (
+    <View>
+      <Text>Where the favorites and can live when marked along the tour.</Text>
+      <Text>This will be a nice touch that users can keep the images of the homes they loved most.</Text>
+    </View>
+    );
+  }
+}
+
 class UserScreen extends React.Component {
   static navigationOptions = ({ navigation }) => { 
     title: 'User Profile for ${navigation.state.params.user}'
@@ -65,15 +126,20 @@ class UserScreen extends React.Component {
   return (
     <View>
       <Text>Well hello, {params.user}</Text>
+      <Text />
+      <Text>Thinking we could put a list of stops TO VISIT and those VISITED (tab navigation here)?</ Text><Text> Thoughts?</Text>
     </View>
     );
   }
 }
 
 const TourAppCRNA = StackNavigator({
-  Home: { screen: HomeScreen },
+  Welcome: { screen: WelcomeScreen },
   User: { screen: UserScreen},
   Friends: {screen: FriendsScreen },
+  Sponsors: {screen: SponsorsScreen },
+  Homes: {screen: HomesScreen },
+  Favorites: {screen: FavoritesScreen },
   Map: { screen: MapScreen },
 });
 
